48db363b55954a1a9e8568ceb267284812b765c6
crazylazycode <crazytobeawake@gmail.com>
2021-04-10 15:59:56 +0530

    second user answer bug fixing

--- index.js
+++ index.js
@@ -1,40 +1,36 @@
 const app = require("express")();
-const server =  require("http").createServer(app);
+const server = require("http").createServer(app);
 const cors = require("cors");
 
 const io = require("socket.io")(server, {
-    cors:{
-        origin:"*",
-        methods:["GET","POST"]
-    }
+	cors: {
+		origin: "*",
+		methods: [ "GET", "POST" ]
+	}
 });
 
 app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 
-app.get("/",(req,res) => {
-    res.send("Server is Running");
+app.get('/', (req, res) => {
+	res.send('Running');
 });
 
+io.on("connection", (socket) => {
+	socket.emit("me", socket.id);
 
-io.on("connection",(socket) => {
-    //getting my id 
-    socket.emit("me",socket.id);
-    
-    //for disconnection
-    socket.on("disconnect", () => {
-        socket.broadcast.emit("callEnded");
-    })
+	socket.on("disconnect", () => {
+		socket.broadcast.emit("callEnded")
+	});
 
-    socket.on("callUser",({userToCall,signalData,from,name}) => {
-        io.to(userToCall).emit("callUser",{ signal: signalData, from,name});
-    });
+	socket.on("callUser", ({ userToCall, signalData, from, name }) => {
+		io.to(userToCall).emit("callUser", { signal: signalData, from, name });
+	});
 
-    socket.on("answercall", (data) => {
-        io.to(data.to).emit("callAccepted",data.signal)
-    })
-    
-})
+	socket.on("answerCall", (data) => {
+		io.to(data.to).emit("callAccepted", data.signal)
+	});
+});
 
-server.listen(PORT,() => console.log(`Server is running on ${PORT}`))
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
